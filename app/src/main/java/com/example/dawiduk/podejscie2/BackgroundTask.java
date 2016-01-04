bba314d2c4062fcd3f3d2973e9209df4830f855d
pdawiduk <pdawiduk@gmail.com>
2016-01-04 13:51:17 +0100

    add display image about weather info

--- app/src/main/java/com/example/dawiduk/podejscie2/BackgroundTask.java
+++ app/src/main/java/com/example/dawiduk/podejscie2/BackgroundTask.java
@@ -38,7 +38,33 @@ public class BackgroundTask extends AsyncTask<String, Void, Void> {
     private ForecastAdapter adapter;
     private Context context;
 
+    public static int getIconResourceForWeatherCondition(int weatherId) {
 
+        if (weatherId >= 200 && weatherId <= 232) {
+            return R.drawable.ic_storm;
+        } else if (weatherId >= 300 && weatherId <= 321) {
+            return R.drawable.ic_light_rain;
+        } else if (weatherId >= 500 && weatherId <= 504) {
+            return R.drawable.ic_rain;
+        } else if (weatherId == 511) {
+            return R.drawable.ic_snow;
+        } else if (weatherId >= 520 && weatherId <= 531) {
+            return R.drawable.ic_rain;
+        } else if (weatherId >= 600 && weatherId <= 622) {
+            return R.drawable.ic_snow;
+        } else if (weatherId >= 701 && weatherId <= 761) {
+            return R.drawable.ic_fog;
+        } else if (weatherId == 761 || weatherId == 781) {
+            return R.drawable.ic_storm;
+        } else if (weatherId == 800) {
+            return R.drawable.ic_clear;
+        } else if (weatherId == 801) {
+            return R.drawable.ic_light_clouds;
+        } else if (weatherId >= 802 && weatherId <= 804) {
+            return R.drawable.ic_cloudy;
+        }
+        return -1;
+    }
 
     long addLocation(String locationSetting, String cityName, double lat, double lon) {
 
